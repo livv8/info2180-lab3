3c8ea327f66787152753f48bfe8d9821ae88a3fb
livv8 <oliviahib898@gmail.com>
2021-10-10 17:46:06 -0500

    Added file

---
+++ tic-tac-toe.js
@@ -0,0 +1,20 @@
+document.getElementById("board").classList.add("square"); 
+
+var board = document.getElementById('board');
+window.addEventListener("DOMContentLoaded", function(event) {board.classList.add('square');
+ });
+
+ function  event(e){
+     console.log('clicked')
+ }
+//boardElements.forEach( div => {div.addEventListener('click', handleClick, {once :true})})
+
+
+/*var victory = new Array();
+var firstplayer = new Array();
+var secondplayer = new Array ();
+var currentplayer = 0;
+var pointsfirst = 0;
+var pointssec= 0;
+var size = 3;
+*/
