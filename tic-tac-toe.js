c1442311d7dc1cdf21a2b02d51d660095c5cdabe
livv8 <oliviahib898@gmail.com>
2021-10-10 23:35:23 -0500

    Js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,20 @@
-//const button= 
+const X_input 
+const O_input
+var Victory= new Array();
 const boardElements = document.querySelectorAll('#board');
 boardElements.forEach(childNodes => {childNodes.classList.add("square")});
 
 document.addEventListener("DOMContentLoaded",  () =>{
     let btn=document.getElementsByClassName('btn');
-    btn.addEventListener('click', ()=> { console.log("clicked"); 
+    btn.addEventListener('click', ()=> { console.log(childNodes); 
     });
 });
+
+function turns(){
+    X_input=document.classList.add("square.X");
+    O_input=document.classList.add("square.O");
+    let x= document.getElementById("#board"    
+    );
+    x.textContent = " X"
+    O_input.textContent= "O"
+}
