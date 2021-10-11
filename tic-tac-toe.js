a834378f47e62b202ef2ca80de418175c5760b3f
livv8 <oliviahib898@gmail.com>
2021-10-10 23:10:31 -0500

    Js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,9 @@
-document.getElementById("board").classList.add("square"); 
+//const button= 
+const boardElements = document.querySelectorAll('#board');
+boardElements.forEach(childNodes => {childNodes.classList.add("square")});
 
-var board = document.getElementById('board');
-window.addEventListener("DOMContentLoaded", function(event) {board.classList.add('square');
- });
-
- function  event(e){
-     console.log('clicked')
- }
-//boardElements.forEach( div => {div.addEventListener('click', handleClick, {once :true})})
-
-
-/*var victory = new Array();
-var firstplayer = new Array();
-var secondplayer = new Array ();
-var currentplayer = 0;
-var pointsfirst = 0;
-var pointssec= 0;
-var size = 3;
-*/
+document.addEventListener("DOMContentLoaded",  () =>{
+    let btn=document.getElementsByClassName('btn');
+    btn.addEventListener('click', ()=> { console.log("clicked"); 
+    });
+});
